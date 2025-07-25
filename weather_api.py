ce1d7a2349ec5c7419ee6fe06a6d2bbe947d7836
ehsan8k-it <ehsanarts580@gmail.com>
2025-07-24 20:50:21 -0700

    This an simple python api fetcher

---
+++ weather_api.py
@@ -0,0 +1,20 @@
+import requests
+# api key you can generate from openweathermap.org
+api_key = "d0efa552d653d91a1b9859d0e20cc5b9"
+
+# Latitude AND longitude of your area You can get it from gpt or Google
+lat = 28.96395
+lon = 69.06849
+city = "Zain koh loti"
+
+# This is api url of whether forcast
+url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={api_key}&units=metric"
+
+# getting data from url
+res = requests.get(url)
+# Converting that data into readable JSON formate.
+data = res.json()
+weather_description = data["weather"][0]["description"]
+
+print(f"The weather of {city} is: {weather_description}")
+
